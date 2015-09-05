930cedd536a5cefa1c7bf1b79419236d8a5c298f
Andre Keys <andrekeys@u.boisestate.edu>
2015-09-05 16:41:55 -0600

    using default express generated app

--- app/app.js
+++ app/app.js
@@ -1,22 +1,60 @@
-var express = require('express'),
-    http = require('http'),
-    log4js = require('log4js');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
-var log = log4js.getLogger();
 
-var serverInfo = {
-    port: 3000
-};
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var server = app.listen(serverInfo.port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    log.info('App listening on http://%s:%s', host, port);
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/', function (req, res) {
-    res.send('hello world!');
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
+
+module.exports = app;
