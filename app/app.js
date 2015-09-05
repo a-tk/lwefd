fc1b2e05c097a52f3ab4c108f28b1b820131bd4c
Andre Keys <andrekeys@u.boisestate.edu>
2015-09-05 17:32:33 -0600

    cleaned up and removed some articles

--- app/app.js
+++ app/app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var reqLogger= require('morgan');
+var log4js = require('log4js');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var log = log4js.getLogger();
+var serverConfig = {
+  port: 3000
+};
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,26 +21,47 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(reqLogger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ *
+ * Routes
+ *
+ */
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+
+/**
+ *
+ * Status Codes
+ *
+ */
+
+/**
+ * 404 - forward to error handler
+ */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
+
+/**
+ *
+ * Error Handlers
+ *
+ */
+
+/**
+ * Development Error handler
+ */
+log.info("app.get('env') = " + app.get('env'));
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +72,9 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+/**
+ * Production Error Handler
+ */
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -56,5 +83,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var server = app.listen(serverConfig.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  log.info('lwefd listening at http://'+ host + ':' + port)
+});
 
 module.exports = app;
