5edbe11dff3d28c9d7288016dfe143a68549c4f9
Andre Keys <andrekeys@u.boisestate.edu>
2015-09-08 20:45:28 -0600

    new singleton pattern implemented. The unit tests are invavalid?

--- app/util/Notifications/NotificationQueue.js
+++ app/util/Notifications/NotificationQueue.js
@@ -1,20 +1,3 @@
-var log4js = require('log4js'),
-  log = log4js.getLogger('Notification Queue');
-/*
-function NotificationQueue () {
-
-  if (arguments.callee._singletonInstance) {
-    log.warn('Cannot instantiate more than one NotificationQueue');
-    return arguments.callee._singletonInstance;
-  }
-  else {
-    arguments.callee._singletonInstance = this;
-    log.info('NotificationQueue Created');
-    //functions go here
-    //this.addNotification = function (notification) {
-  }
-}
-*/
 
 var NotificationQueue = function () {
   var log4js = require('log4js'),
