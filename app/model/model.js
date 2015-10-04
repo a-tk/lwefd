3ea95ad76d3b816343cce49e3eaf65f6995d1cbd
Andre Keys <andrekeys@u.boisestate.edu>
2015-10-04 15:56:59 -0600

    Added some sparse model operations based on playing with then mongo shell

--- app/model/model.js
+++ app/model/model.js
@@ -3,6 +3,8 @@ var model = (function () {
     assert = require('assert');
 
   var db = null;
+  var products = 'products';
+  var jobs = 'jobs';
 
   var url = 'mongodb://localhost:27017/lwefd';
   var connect = function () {
@@ -13,12 +15,47 @@ var model = (function () {
     });
   };
 
+  var addProduct = function (id, name, displayName, callback) {
+    db.collection(products).insertOne( {
+      _id : id,
+      name : name,
+      displayName : displayName
+    }, function (err, result) {
+      assert.equal(null, err);
+      console.log('added product');
+      callback(result);
+    });
+  };
+
+  var addRun = function (productId, name, status, callback) {
+    db.collection(jobs).updateOne( {
+      productId : productId,
+      name : name,
+      runs: [
+
+      ]
+    }, {
+        $push : {
+          runs : {
+            date: Date.now(),
+            status : status
+          }
+        }
+      }, {upsert: true}).then(function (err, result) {
+      assert.equal(null, err);
+      console.log('added run');
+      callback(result);
+    });
+  };
+
   var close = function() {
     db.close();
   };
 
   return {
     connect: connect,
+    addProduct: addProduct,
+    addRun : addRun,
     close: close
   }
 })();
