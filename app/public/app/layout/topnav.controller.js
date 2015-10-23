4f551c8318026e5e5fed72068760370cd9683153
Andre Keys <andrekeys@u.boisestate.edu>
2015-10-22 21:04:38 -0600

    adding frontend

--- app/public/app/layout/topnav.controller.js
+++ app/public/app/layout/topnav.controller.js
@@ -4,16 +4,17 @@
     angular.module('app')
         .controller('TopNavCtrl', TopNavCtrl);
 
-    TopNavCtrl.$inject = ['$route'];
+    TopNavCtrl.$inject = ['$route', '$location'];
 
-    function TopNavCtrl($route) {
+    function TopNavCtrl($route, $location) {
         /* jshint validthis:true */
         var vm = this;
 
         vm.isRoute = isRoute;
 
         function isRoute(r) {
-            return $route.current.title.substr(0, r.length) === r;
+            //return $route.current.title.substr(0, r.length) === r;
+            return $location.path() === r;
         }
     }
 })();
