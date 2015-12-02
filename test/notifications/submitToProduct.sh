4115c31c74b4bbee48561287d43368ea73920c53
Andre Keys <andrekeys@u.boisestate.edu>
2015-12-01 21:45:55 -0700

    Finally just a script for submitting whatever I want

---
+++ test/notifications/submitToProduct.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#HOST="lwefd.me"
+HOST="localhost:3000"
+
+if [ $# -ne 5 ]; then
+
+  echo "usage: $0 <name> <number> [STARTED | COMPLETED] [FAILURE|UNSTABLE|SUCCESS|STARTED] <pid>"
+
+else
+  DATA="{\"name\":\"${1}\", \"build\": {\"full_url\":\"http://lwefd.me\", \"number\": ${2}, \"phase\":\"${3}\", \"status\":\"${4}\"}}"
+  echo ${DATA}
+  echo curl "http://${HOST}/api/${2}/notify" --header "Content-Type: application/json" -X POST --data ${DATA} &
+  curl "http://${HOST}/api/${2}/notify" --header "Content-Type: application/json" -X POST --data "${DATA}" &
+fi
+
