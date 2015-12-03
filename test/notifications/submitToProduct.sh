cbb377ab1fc8447250d44ed289eda29024d22663
Andre Keys <andrekeys@u.boisestate.edu>
2015-12-02 21:02:08 -0700

    better script and ordering by most recent

--- test/notifications/submitToProduct.sh
+++ test/notifications/submitToProduct.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-#HOST="lwefd.me"
-HOST="lwefd.me"
+if [ $# -ne 6 ]; then
 
-if [ $# -ne 5 ]; then
-
-  echo "usage: $0 <name> <number> [STARTED | COMPLETED] [FAILURE|UNSTABLE|SUCCESS|STARTED] <pid>"
+  echo "usage: $0 <name> <number> [STARTED | COMPLETED] [FAILURE|UNSTABLE|SUCCESS|STARTED] <pid> <host>"
 
 else
   DATA="{\"name\":\"${1}\", \"build\": {\"full_url\":\"http://lwefd.me\", \"number\": ${2}, \"phase\":\"${3}\", \"status\":\"${4}\"}}"
   echo ${DATA}
-  echo curl "http://${HOST}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data ${DATA} &
-  curl "http://${HOST}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data "${DATA}" &
+  echo curl "http://${6}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data ${DATA} &
+  curl "http://${6}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data "${DATA}" &
 fi
 
