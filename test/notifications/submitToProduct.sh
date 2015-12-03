e9a8bbc5bb886a4e4171d0fd2ba0ec15ee912140
Andre Keys <andrekeys@u.boisestate.edu>
2015-12-02 18:33:39 -0700

    Modifications making it work correctly

--- test/notifications/submitToProduct.sh
+++ test/notifications/submitToProduct.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #HOST="lwefd.me"
-HOST="localhost:3000"
+HOST="lwefd.me"
 
 if [ $# -ne 5 ]; then
 
@@ -10,7 +10,7 @@ if [ $# -ne 5 ]; then
 else
   DATA="{\"name\":\"${1}\", \"build\": {\"full_url\":\"http://lwefd.me\", \"number\": ${2}, \"phase\":\"${3}\", \"status\":\"${4}\"}}"
   echo ${DATA}
-  echo curl "http://${HOST}/api/${2}/notify" --header "Content-Type: application/json" -X POST --data ${DATA} &
-  curl "http://${HOST}/api/${2}/notify" --header "Content-Type: application/json" -X POST --data "${DATA}" &
+  echo curl "http://${HOST}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data ${DATA} &
+  curl "http://${HOST}/api/${5}/notify" --header "Content-Type: application/json" -X POST --data "${DATA}" &
 fi
 
