6f5b3c66914ec895b9264f6f340a014562dd5f6d
andrekeys <andre.t.keys@hp.com>
2016-02-18 13:24:52 -0700

    Do not perform Modulus on hour count, leave it in total hours.

--- client/scripts/controllers/summary.controller.js
+++ client/scripts/controllers/summary.controller.js
@@ -33,7 +33,7 @@
       return function(duration) {
           var seconds = parseInt((duration/1000)%60)
           , minutes = parseInt((duration/(1000*60))%60)
-          , hours = parseInt((duration/(1000*60*60))%24);
+          , hours = parseInt((duration/(1000*60*60)));
 
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
